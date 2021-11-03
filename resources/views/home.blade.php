1b8e5b60fa8b176f5852a341617cb184426c08b7
Ian Cowan <ian@cowanemail.com>
2021-11-02 21:57:51 -0400

    Remove semicolons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,9 +16,9 @@
                     <a target="_blank" href="https://www.github.com/iccowan"><i class="fab fa-github"></i></a> | <a href="mailto:ian@cowanemail.com"><i class="far fa-envelope"></i></a> | <a target="_blank" href="https://www.youtube.com/channel/UCfEj4FfmymrdtcENSrRbdhw"><i class="fab fa-youtube"></i></a>
                     <br />
                     <i>
-                        <h5>Commercial Pilot ASEL/IR;</h5>
-                        <h5>CFI-I, AGI, IGI;</h5>
-                        <h5>Student at Centre College (Mathematics/Computer Science);</h5>
+                        <h5>Commercial Pilot ASEL/IR</h5>
+                        <h5>CFI-I, AGI, IGI</h5>
+                        <h5>Student at Centre College (Mathematics/Computer Science)</h5>
                         <h5>Software Engineer</h5>
                     </i>
                 </div>
