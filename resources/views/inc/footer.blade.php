defbc87d72aacec21d74cccb8cb83aa5af90f5e4
Ian Cowan <ian@cowanemail.com>
2021-11-03 10:39:49 -0400

    Copyright year 2019 -> 2021

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <a href="/">Home</a> | <a href="/github">GitHub Repositories</a> | <a href="/youtube">YouTube Content</a>
     <hr>
-    &copy;2019 by Ian Cowan
+    &copy;2021 by Ian Cowan
     <br>
     <a target="_blank" href="https://www.github.com/iccowan"><i class="fab fa-github"></i></a> | <a href="mailto:ian@cowanemail.com"><i class="far fa-envelope"></i></a> | <a target="_blank" href="https://www.youtube.com/channel/UCfEj4FfmymrdtcENSrRbdhw"><i class="fab fa-youtube"></i></a>
 </footer>
