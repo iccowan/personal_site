4a4cd2cdefd93c427ad34d47b2739e77f3179d4f
Ian Cowan <ian@cowanemail.com>
2021-11-02 21:52:14 -0400

    Realign titles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
         <div class="content-block">
             <div class="blue">
                 <center>
-                    <i><h2>How I got into Aviation...</h2></i>
+                    <i><h2 class="align-right">How I got into Aviation</h2></i>
                     <br>
                     <h3>Ever since I was a small boy I have been flying with my parents on airline flights. I have always wanted to become a pilot myself, but I was not sure how to do so. In December 2017, I flew a Cessna 172 for the first time and I was instantly hooked. Since then, I have worked consistently to gain my commercial pilot certificate with an instrument rating along with becoming a certificated flight instructor with an instrument rating. I love aviation and helping students be successful beginning their flying careers is truly priceless.</h3>
                 </center>
@@ -37,16 +37,16 @@
         <div class="content-block">
             <div class="gray">
                 <center>
-                    <i><h2>My Programming History...</h2></i>
+                    <i><h2 class="align-right">My Programming History</h2></i>
                     <br>
                     <h3>I have always been fascinated by computers and the internet, and I began programming as a hobby in high school. Once I started college, I took my first formal computer science course and was instantly hooked on the computer science courses! Over the last few years, I have worked on a few personal projects along with the course required projects. Along with these personal projects I have worked at Simply Binary in Greenville, SC as an intern and also at Centre College as a student programmer. Feel free to take a look at my GitHub for a few of my recent projects! In the future I hope to get more involved in open source projects and begin a career as a software engineer.</h3>
                 </center>
             </div>
         </div>
         <div class="content-block">
-            <div class="gray">
+            <div class="blue">
                 <center>
-                    <i><h2>My Education...</h2></i>
+                    <i><h2 class="align-right">My Education</h2></i>
                     <br>
                     <h3>I graduated high school from T.C. Roberson High School in Asheville, NC in 2018. I am currently a Computer Science and Mathematics double major at Centre College in Danville, KY with an expected graduation date of May 2022. While at Centre, I have taken many different courses in Math and Computer Science including, but not limited to, Abstract Algebra I/II, Differential Equations, Numerical Methods, Software Design, Artifical Intelligence, and Databases.</h3>
                 </center>
